eb8b106043c23e8409b952f5fe1d0743244d1ab3
Popa Alexandru Vasile <popavasile02@yahoo.com>
2024-02-23 16:28:21 +0200

    Add Initial Spring Template

---
+++ pizzahubapi/src/main/java/com/learning/pizzahubapi/PizzahubapiApplication.java
@@ -0,0 +1,13 @@
+package com.learning.pizzahubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzahubapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzahubapiApplication.class, args);
+	}
+
+}
