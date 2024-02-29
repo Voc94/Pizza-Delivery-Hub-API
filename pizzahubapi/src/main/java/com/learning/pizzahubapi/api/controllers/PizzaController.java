e096c0ce70f2a87e3767f541d92960f02b99addd
Popa Alexandru Vasile <popavasile02@yahoo.com>
2024-03-01 01:59:12 +0200

    added Error Messages and A Cleaner Interface

--- pizzahubapi/src/main/java/com/learning/pizzahubapi/api/controllers/PizzaController.java
+++ pizzahubapi/src/main/java/com/learning/pizzahubapi/api/controllers/PizzaController.java
@@ -1,12 +1,12 @@
 package com.learning.pizzahubapi.api.controllers;
 
+import com.learning.pizzahubapi.api.data.PizzaData;
 import com.learning.pizzahubapi.api.models.Pizza;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,26 +14,40 @@ import java.util.List;
 @Controller
 @RequestMapping("pizza")
 public class PizzaController {
-    public static List<Pizza> pizzas = new ArrayList<>();
-    static {
-        pizzas.add(new Pizza("Margherita", "Bucatar Coco"));
-    }
-
     @GetMapping
     public String displayPizzaTable(Model model){
         model.addAttribute("title","All The Pizza");
-        model.addAttribute("pizzas",pizzas);
+        model.addAttribute("pizzas", PizzaData.getAll());
         return "pizza/index";
     }
     @GetMapping("create")
     public String displayCreatePizzaForm(Model model){
         model.addAttribute("title","Create event");
+        model.addAttribute(new Pizza());
         return "pizza/create";
     }
     @PostMapping("create")
-    public String processCreatePizzaForm(@RequestParam String pizzaName, @RequestParam String pizzaMaker) {
-        pizzas.add(new Pizza(pizzaName, pizzaMaker));
+    public String processCreatePizzaForm(@ModelAttribute @Valid Pizza newPizza, Errors errors,Model model) {
+        if(errors.hasErrors()){
+            model.addAttribute("title","Create event");
+            return "pizza/create";
+        }
+        PizzaData.add(newPizza);
         return "redirect:/pizza"; // Ensure this redirects to the intended view.
     }
-
+    @GetMapping("delete")
+    public String displayDeletePizzaForm(Model model){
+        model.addAttribute("title","Delete Events");
+        model.addAttribute("pizzas",PizzaData.getAll());
+        return "pizza/delete";
+    }
+    @PostMapping("delete")
+    public String processDeletePizzaForm(@RequestParam(required = false) Long[] pizzaIds){
+        if(pizzaIds != null) {
+            for (Long id : pizzaIds) {
+                PizzaData.remove(id);
+            }
+        }
+        return "redirect:/pizza";
+    }
 }
--- pizzahubapi/src/main/java/com/learning/pizzahubapi/api/models/Pizza.java
+++ pizzahubapi/src/main/java/com/learning/pizzahubapi/api/models/Pizza.java
@@ -1,32 +1,63 @@
 package com.learning.pizzahubapi.api.models;
 
+import jakarta.annotation.Nonnull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.bind.Name;
 
+import java.util.Objects;
 @Getter
 public class Pizza {
     private Long id;
     private static Long nextId = 1L;
     @Setter
+    @Valid
+    @NotNull(message = "Name is mandatory")
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 2,max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
     @Setter
+    @NotNull(message = "Chef is mandatory")
+    @NotBlank(message = "Chef is mandatory")
+    @Size(min = 3,max = 50, message = "Chef must be between 3 and 50 characters")
     private String madeBy;
 
     public Pizza(String name, String madeBy) {
         this.id = nextId;
         this.name = name;
         this.madeBy = madeBy;
-        nextId++;
+      //  nextId++;
     }
     public Pizza(){
-
+        this.id = nextId;
+        nextId++;
     }
+
 
     @Override
     public String toString() {
         return "Pizza{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", madeBy='" + madeBy + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pizza pizza = (Pizza) o;
+        return Objects.equals(id, pizza.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
